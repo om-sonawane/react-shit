9edddc5996d0ab4c011beee67a9fc47cd72c622a
OM SONAWANE <omsonawane03@gmail.com>
2025-01-13 18:25:12 +0530

    "new repo"

---
+++ app.js
@@ -0,0 +1,21 @@
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+       [ React.createElement(
+            "h1",
+            {id:"heading"},
+            "I am h1 tag"
+        ),
+        React.createElement(
+            "h2",
+            {id:"heading2"},
+            "I am h2 tag"
+        )]
+)
+
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
