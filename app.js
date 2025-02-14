911e3c2c2c89f56ca51047b3ced8548e7922a8c8
OM SONAWANE <omsonawane03@gmail.com>
2025-02-14 16:44:34 +0530

    "parcel-cache"

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     {id:"parent"},
@@ -19,3 +22,4 @@ const parent = React.createElement(
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+var a=10
+
+function ab(params) {
+    var a=10
+}
+
+ab();
